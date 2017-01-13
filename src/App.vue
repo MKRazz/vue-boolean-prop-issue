1ffe14c4ac01f5a0fda2343378a23f6b79ce7bae
Matt <mk@razzinteractive.com>
2017-01-13 16:05:52 -0500

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <example-component example=""></example-component>
+    </div>
+</template>
+
+<script>
+    import ExampleComponent from './ExampleComponent.vue';
+
+    export default {
+        components: {ExampleComponent},
+    }
+</script>
---
+++ src/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <pre>{{example}}</pre>
+</template>
+
+<script>
+    export default {
+        props: {
+            example: Boolean,
+        },
+    }
+</script>
